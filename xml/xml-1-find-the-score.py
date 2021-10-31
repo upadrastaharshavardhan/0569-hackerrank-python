701d3fdfc23703c243641c6dfafaf0df30291a99
Harsha69 <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-10-31 21:33:04 +0530

    Create xml-1-find-the-score.py

---
+++ xml/xml-1-find-the-score.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as etree
+
+
+def get_attr_number(node):
+    result = 0
+    for child in node:
+        result += get_attr_number(child)
+    return result + len(node.attrib)
+
+
+if __name__ == '__main__':
+    xml = ''
+    for _ in range(int(input())):
+        xml += input()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+
+    print(get_attr_number(root))
