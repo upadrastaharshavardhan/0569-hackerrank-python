1d4cf4bf15d75fd86fe9abe1b35d5b81f07fcdb3
Harsha69 <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-10-31 21:17:04 +0530

    Create array-mathematics.py

---
+++ numpy-python/array-mathematics.py
@@ -0,0 +1,19 @@
+import numpy
+
+N, M = map(int, input().split())
+A = []
+for _ in range(N):
+    A.append(list(map(int, input().split())))
+A = numpy.array(A)
+
+B = []
+for _ in range(N):
+    B.append(list(map(int, input().split())))
+B = numpy.array(B)
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+print(A ** B)
