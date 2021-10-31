f4ffd5af3cd6f37416e9ddda51fb5014c3c58957
Harsha69 <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-10-31 21:13:38 +0530

    Create compress-the-string.py

---
+++ itertools/compress-the-string.py
@@ -0,0 +1,12 @@
+import itertools
+
+line = input()
+result = ''
+
+for entry in itertools.groupby(line):
+    count = 0
+    for element in entry[1]:
+        count += 1
+    result += str((count, int(entry[0]))) + ' '
+
+print(result)
