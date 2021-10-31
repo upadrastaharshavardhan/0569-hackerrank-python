6ae022f6df66ee32849b9e3a8f123ba9b595731c
Harsha69 <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-10-31 21:21:58 +0530

    Create detect-floating-point-numbers.py

---
+++ regex-and-parsing/detect-floating-point-numbers.py
@@ -0,0 +1,18 @@
+import re
+
+pattern = '([+|-]){0,1}(\d)*[.](\d)+'
+
+
+def is_float(number):
+    try:
+        float(number)
+        info = re.match(pattern, number)
+        return info is not None and info.span() == (0, len(number))
+    except ValueError as _:
+        return False
+
+
+queries = int(input())
+for _ in range(queries):
+    number = input()
+    print(is_float(number))
