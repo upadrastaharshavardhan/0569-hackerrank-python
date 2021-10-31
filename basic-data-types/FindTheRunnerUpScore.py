6e5cb133dcdae12246a1e12d9a91d8f9fe9a0bb2
Harsha69 <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-10-31 20:58:00 +0530

    Create FindTheRunnerUpScore.py

---
+++ basic-data-types/FindTheRunnerUpScore.py
@@ -0,0 +1,14 @@
+def second_highest(array):
+    array.sort()
+    maximum = array[len(array) - 1]
+    for i in range(len(array)):
+        if array[len(array) - 1 - i] < maximum:
+            return array[len(array) - 1 - i]
+
+
+n = int(input())
+array = list(map(int, input().split()))
+print(second_highest(array))
+© 2021 GitHub, Inc.
+Terms
+Priv
